64735d6312ec7a929f5d37754d578deca924cb0c
benedictFaw <benedict@fawcett.co.uk>
2022-10-28 14:35:08 +0100

    added make compile

--- Makefile
+++ Makefile
@@ -1,1 +1,7 @@
+all: perimeter.o main.c
+	gcc -o program main.c perimeter.o
+
+perimeter.o: perimeter.c perimeter.h
+	gcc -c perimeter.c
+
 
--- perimeter.c
+++ perimeter.c
@@ -1,6 +1,24 @@
 #include "perimeter.h"
 
 ull perimeter(int n) {
-  return 0;
+  int perimeter = 0;
+  if(n == 0){
+    perimeter = 4;
+  }
+  else{
+    int nums[n+1];
+    nums[0] = 1;
+    nums[1] = 1;
+    for( int x =2; x <= n+1 ; x++){
+      nums[x] = nums[x-1] + nums[x-2];
+    }
+    for(int loop = 0; loop < 10; loop++){
+    } 
+    int size = (sizeof(nums))/(sizeof(nums[0]));
+    for( int j=0; j < size; j ++){
+      perimeter = perimeter + 4*(nums[j]);
+    }
+  }
+  
+  return perimeter;
 }
-
